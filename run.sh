d8ad168ecc27767aec4b315d0573a7697dd9e02b
Marek Sebera <marek.sebera@gmail.com>
2016-05-29 17:27:29 +0200

    Update scripts to provide Developer Sandbox automatically

--- run.sh
+++ run.sh
@@ -2,4 +2,15 @@
 
 BASEDIR=$(dirname $0)
 
+export CHROME_DEVEL_SANDBOX=$BASEDIR/latest/chrome_sandbox
+
+# Uncomment these lines to get rid of API Keys missing warning
+#export GOOGLE_API_KEY="no"
+#export GOOGLE_DEFAULT_CLIENT_ID="no"
+#export GOOGLE_DEFAULT_CLIENT_SECRET="no"
+
+# Use this if you want separate profile location
 $BASEDIR/latest/chrome --user-data-dir="$BASEDIR/user-data-dir" $* &> /dev/null &
+
+# Use this if you want to use standard profile location
+# $BASEDIR/latest/chrome-wrapper $* &> /dev/null &
