a76837b00f9fb10f2b2c26311ff95a8ae6eab12d
sudo-Eze <84420737+sudo-Eze@users.noreply.github.com>
2021-05-19 13:15:07 -0300

    Add the remove message of Google APIs
    Add the remove message of Google APIs and Add in comments the option of open when the update is finalized

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
-BASEDIR=$(dirname $0)
-
-$BASEDIR/latest/chrome --user-data-dir="$BASEDIR/user-data-dir" $* &> /dev/null &
+#if [ -e "$HOME/.Chromium/State" ]; then
+#    notify-send -t 20000 -i "$HOME/.Chromium/Chromium Update.png" "Updating Chromium..." "Chromium is currently being updated. Please Wait... It Will Open When This Finalize."
+#    touch "$HOME/.Chromium/State/Requested"
+#    exit 0
+#else
+    #REMOVE THE MESSAGE FROM GOOGLE APIs
+        export GOOGLE_API_KEY="no"
+        export GOOGLE_DEFAULT_CLIENT_ID="no"
+        export GOOGLE_DEFAULT_CLIENT_SECRET="no"
+    BASEDIR=$(dirname $0)
+    $BASEDIR/latest/chrome --user-data-dir="$BASEDIR/user-data-dir" $* &> /dev/null &
+#fi
