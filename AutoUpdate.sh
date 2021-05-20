df8c61168d265a7bcab0edf1f2c3442e6cd53519
sudo-Eze <84420737+sudo-Eze@users.noreply.github.com>
2021-05-20 15:05:37 -0300

    Fix AutoUpdate Problem

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd $(dirname $0)
-LAST_UPDATE=$(cat Ultima_Actualizacion)
+LAST_UPDATE=$(cat Update_Data/Last_AutoUpdate)
 DATE=$(date +%j)
 
 if [ "$LAST_UPDATE" = "$DATE" ]; then
@@ -9,5 +9,5 @@ if [ "$LAST_UPDATE" = "$DATE" ]; then
 else
     sleep 20
     bash ./update.sh
-    echo "$DATE" > Ultima_Actualizacion
+    echo "$DATE" > Update_Data/Last_AutoUpdate
 fi
