bef29bfb07a52f0711f10e49e9f795efd9e11181
sudo-Eze <84420737+sudo-Eze@users.noreply.github.com>
2021-05-20 15:42:23 -0300

    Add Autorecognition of the SO and Fix some errors

--- run.sh
+++ run.sh
@@ -1,29 +1,32 @@
 #! /bin/bash
 
-#if [ -e "$HOME/.Chromium/State/UPDATING" ]; then
-#    notify-send -t 20000 -i "$HOME/.Chromium/Chromium Update.png" "Updating Chromium..." "Chromium is currently being updated. Please Wait... It Will Open When This Finalize."
-#    touch "$HOME/.Chromium/State/REQUESTED"
-#    exit 0
-#else
-    #REMOVE THE MESSAGE FROM GOOGLE APIs
-    
+KernelOS=$(uname)
+
+if [ -e "State/UPDATING" ]; then
+    if [ $KernelOS == "Linux" ]; then notify-send -t 20000 -i "$HOME/.Chromium/Chromium Update.png" "Updating Chromium..." "Chromium is currently being updated. Please Wait... It Will Open When This Finalize." ; fi
+    if [ $KernelOS == "Darwin" ]; then osascript -e 'display notification "Chromium is currently being updated. Please Wait... It Will Open When This Finalize." with title "Updating Chromium..." sound name "Submarine"' ; fi
+    touch "$HOME/.Chromium/State/REQUESTED"
+    exit 0
+else
+        #REMOVE THE MESSAGE FROM GOOGLE APIs
         export GOOGLE_API_KEY="no"
         export GOOGLE_DEFAULT_CLIENT_ID="no"
         export GOOGLE_DEFAULT_CLIENT_SECRET="no"
     BASEDIR=$(dirname $0)
     
     $BASEDIR/latest/chrome --user-data-dir="$BASEDIR/user-data-dir" $* &> /dev/null &
-#fi
+fi
 
-case "$1" in
-  linux)
+case "$KernelOS" in
+  Linux)
     $BASEDIR/latest/chrome --user-data-dir="$BASEDIR/user-data-dir" $* &> /dev/null &
     ;;
-  mac)
+  Darwin)
     open $BASEDIR/latest/chromium.app
     ;;
   *)
-    echo "Please specify platform (mac or linux) as argument.";
-    exit 1
+    echo "Error identifying the Operating System. Your current system is: $KernelOS and it is not specified in the functions of this script. Please, Contact to the developer on GitHub";
+    read RESPONSE
+    exit
     ;;
 esac
