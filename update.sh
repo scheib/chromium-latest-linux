e1aa7af0dabf735fabaea4e8aaea0c44d494be44
sudo-Eze <84420737+sudo-Eze@users.noreply.github.com>
2021-05-20 15:03:56 -0300

    Add Mac Support

--- update.sh
+++ update.sh
@@ -2,35 +2,57 @@
 
 cd $(dirname $0)
 
-#COMPRUEBA QUE NO ESTÉ ACTUALIZANDO
-if [ -e "./State/UPDATING" ]; then
-    notify-send -t 20000 -i "$(pwd)/Icons/Chromium Update.png" "Updating Chromium..." "Chromium is currently being updated ..."
+#CHECK THAT IS NOT ALREDY UPDATING
+if [ -e "State/UPDATING" ]; then
+    if [ $1 == "mac" ]; then osascript -e 'display notification "Chromium is currently being updated ..." with title "Updating Chromium..." sound name "Submarine"' ; fi
+    if [ $1 == "linux" ]; then notify-send -t 20000 -i "$(pwd)/Icons/Chromium Update.png" "Updating Chromium..." "Chromium is currently being updated ..." ; fi
+    exit
+fi
+
+case "$1" in
+  linux)
+    PLATFORM="linux"
+    REVISION_PREFIX="Linux_x64"
+    ;;
+  mac)
+    PLATFORM="mac"
+    REVISION_PREFIX="Mac"
+    ;;
+  *)
+    echo "Please specify platform (mac or linux) as argument.";
+    exit 1
+    ;;
+esac
+
+#CHECK IF IT ISN'T UPDATED
+if [ -e "State/UPDATING" ]; then
+    if [ $1 == "linux" ]; then notify-send -t 20000 -i "$(pwd)/Icons/Chromium Update.png" "Updating Chromium..." "Chromium is currently being updated ..." ; fi
+    if [ $1 == "mac" ]; then osascript -e 'display notification "Chromium is currently being updated ..." with title "Updating Chromium..." sound name "Submarine"' ; fi
     exit 0
 fi
 
-LASTCHANGE_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2FLAST_CHANGE?alt=media"
+LASTCHANGE_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/${REVISION_PREFIX}%2FLAST_CHANGE?alt=media"
 
 REVISION=$(curl -s -S $LASTCHANGE_URL)
 
-#echo "latest revision is $REVISION"
 
-#COMPRUEBA SI ESTÁ ACTUALIZADO
+#CHECK IF IT'S ALREDY UPDATED
 if [ -d $REVISION ] ; then
-#    echo "already have latest version"
-    notify-send -t 8000 -i "$(pwd)/Icons/Chromium Updated.png" "Updated (Rev.: $REVISION)" "Chromium is in its Latest Version"
+    if [ $1 == "linux" ]; then notify-send -t 8000 -i "$(pwd)/Icons/Chromium Updated.png" "Updated (Rev.: $REVISION)" "Chromium is in its Latest Version" ; fi
+    if [ $1 == "mac" ]; then osascript -e 'display notification "Chromium is in its Latest Version" with title "Updated (Rev.: $REVISION)" sound name "Submarine"' ; fi
   exit
 fi
 
-#CREA REGISTROS Y BLOQUEO
+#CREATE LOGS Y LOCKS
 touch Update_Data/Version
 touch Update_Data/Pre-Version
 touch Update_Data/Pre-Pre-Version
 touch Update_Data/Pre-Pre-Pre-Version
 touch State/UPDATING
 
-ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$REVISION%2Fchrome-linux.zip?alt=media"
+ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/${REVISION_PREFIX}%2F${REVISION}%2Fchrome-${PLATFORM}.zip?alt=media"
 
-ZIP_FILE="${REVISION}-chrome-linux.zip"
+ZIP_FILE="${REVISION}-chrome-${PLATFORM}.zip"
 
 echo "fetching $ZIP_URL"
 
@@ -52,10 +74,11 @@ cat Update_Data/Version > Update_Data/Pre-Version
 echo "$REVISION" > Update_Data/Version
 OLD_VERSION=$(cat Update_Data/Pre-Pre-Pre-Version)
 rm -rf "$OLD_VERSION"
-notify-send -t 20000 -i "$(pwd)/Icons/Chromium Updated.png" "Chromium Updated" "Chromium has been Successfully Updated. Chromium is in its Latest Version ($REVISION)"
 
+if [ $1 == "linux" ]; then notify-send -t 20000 -i "$(pwd)/Icons/Chromium Updated.png" "Chromium Updated" "Chromium has been Successfully Updated. Chromium is in its Latest Version ($REVISION)" ; fi
+if [ $1 == "mac" ]; then ln -s ~/.chromium/latest/Chromium.app/ /Applications/Chromium.app && osascript -e 'display notification "Chromium has been Successfully Updated. Chromium is in its Latest Version ($REVISION)" with title "Chromium Updated" sound name "Submarine"' ; fi
 
-rm ./State/UPDATING
+rm UPDATING
 if [ -e Status/REQUESTED ]; then
     ./run.sh
     rm Status/REQUESTED
