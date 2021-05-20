402fa37748ecc52769d810326d9f187512f6f42e
sudo-Eze <84420737+sudo-Eze@users.noreply.github.com>
2021-05-20 13:17:33 -0300

    Fix some errors

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@
 cd $(dirname $0)
 
 #COMPRUEBA QUE NO ESTÉ ACTUALIZANDO
-if [ -e "State/UPDATING" ]; then
-    notify-send -t 20000 -i "Icons/Chromium Update.png" "Updating Chromium..." "Chromium is currently being updated ..."
+if [ -e "./State/UPDATING" ]; then
+    notify-send -t 20000 -i "./Icons/Chromium Update.png" "Updating Chromium..." "Chromium is currently being updated ..."
     exit 0
 fi
 
@@ -17,7 +17,7 @@ REVISION=$(curl -s -S $LASTCHANGE_URL)
 #COMPRUEBA SI ESTÁ ACTUALIZADO
 if [ -d $REVISION ] ; then
 #    echo "already have latest version"
-    notify-send -t 8000 -i "Icons/Chromium Updated.png" "Updated (Rev.: $REVISION)" "Chromium is in its Latest Version"
+    notify-send -t 8000 -i "./Icons/Chromium Updated.png" "Updated (Rev.: $REVISION)" "Chromium is in its Latest Version"
   exit
 fi
 
@@ -26,7 +26,7 @@ touch Update_Data/Version
 touch Update_Data/Pre-Version
 touch Update_Data/Pre-Pre-Version
 touch Update_Data/Pre-Pre-Pre-Version
-touch "State/UPDATING"
+touch State/UPDATING
 
 ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$REVISION%2Fchrome-linux.zip?alt=media"
 
@@ -55,7 +55,7 @@ rm -rf "$OLD_VERSION"
 notify-send -t 20000 -i "Icons/Chromium Updated.png" "Chromium Updated" "Chromium has been Successfully Updated. Chromium is in its Latest Version ($REVISION)"
 
 
-rm UPDATING
+rm ./State/UPDATING
 if [ -e Status/REQUESTED ]; then
     ./run.sh
     rm Status/REQUESTED
