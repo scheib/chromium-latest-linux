2008ccc528229a5ead9d7b2a7a8e4a9425571ee9
sudo-Eze <84420737+sudo-Eze@users.noreply.github.com>
2021-05-20 07:34:36 -0300

    Add Mac or Linux obligatorie specification

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-#if [ -e "$HOME/.Chromium/State" ]; then
+#if [ -e "$HOME/.Chromium/State/UPDATING" ]; then
 #    notify-send -t 20000 -i "$HOME/.Chromium/Chromium Update.png" "Updating Chromium..." "Chromium is currently being updated. Please Wait... It Will Open When This Finalize."
-#    touch "$HOME/.Chromium/State/Requested"
+#    touch "$HOME/.Chromium/State/REQUESTED"
 #    exit 0
 #else
     #REMOVE THE MESSAGE FROM GOOGLE APIs
@@ -15,9 +15,15 @@
     $BASEDIR/latest/chrome --user-data-dir="$BASEDIR/user-data-dir" $* &> /dev/null &
 #fi
 
-
-if [ $1=="mac" ]; then
-  open $BASEDIR/latest/chromium.app
-else
-  $BASEDIR/latest/chrome --user-data-dir="$BASEDIR/user-data-dir" $* &> /dev/null &
-fi
+case "$1" in
+  linux)
+    $BASEDIR/latest/chrome --user-data-dir="$BASEDIR/user-data-dir" $* &> /dev/null &
+    ;;
+  mac)
+    open $BASEDIR/latest/chromium.app
+    ;;
+  *)
+    echo "Please specify platform (mac or linux) as argument.";
+    exit 1
+    ;;
+esac
