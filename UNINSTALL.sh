fd50de9ec26cdd392e1827a0a3f964f3bd10ef0a
sudo-Eze <84420737+sudo-Eze@users.noreply.github.com>
2021-05-28 19:00:08 -0300

    Delete Chromium Shortcut

--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -7,6 +7,8 @@ if [ -d ~/.chromium ]; then
 else
   rm -rf ~/.chromium 
   sudo rm /usr/bin/chromium
+  if [ $KernelOS == "Darwin" ]; then rm -f /Applications/Chromium.app ; fi
+  if [ $KernelOS == "Linux" ]; then rm -f /usr/share/applications/Chromium.desktop && rm -f /usr/share/applications/Restore\ Chromium.desktop ; fi
   clear
   while [ "$(crontab -l | grep "* * * * 1 $HOME/.chromium/AutoUpdate.sh")" = "* * * * 1 $HOME/.chromium/AutoUpdate.sh" ] ; do
     echo ""
