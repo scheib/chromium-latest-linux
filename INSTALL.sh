6814669cead3ffab689a6d696584bfdf83d87a02
sudo-Eze <84420737+sudo-Eze@users.noreply.github.com>
2021-05-29 11:06:52 -0300

    Fix an error

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,7 +5,7 @@ UserName=$(whoami)
 
 mkdir ~/.chromium/
 
-cp $BASEDIR/Scripts/Remove\ Last\ Version.sh ~/.chromium/
+cp $BASEDIR/Scripts/Remove-Last-Version.sh ~/.chromium/
 cp $BASEDIR/Scripts/run.sh ~/.chromium/
 cp $BASEDIR/Scripts/update.sh ~/.chromium/
 
